fc75d2e8c8428dc79c4a45750254e6c3291881f0
Ruben Conde <rubconqui@gmail.com>
2024-10-16 02:03:46 +0200

    Practica 1 parte 1 acabada

--- Practica 1 MP/Cliente.cpp
+++ Practica 1 MP/Cliente.cpp
@@ -1,1 +1,49 @@
 #include "Cliente.h"
+
+// Constructor
+Cliente::Cliente(int dni, const char* name, const Fecha& fecha) : dni(dni), fechaAlta(fecha) {
+    nombre = new char[strlen(name) + 1];
+    strcpy(nombre, name);
+}
+
+// Constructor por copia
+Cliente::Cliente(const Cliente& other) : dni(other.dni), fechaAlta(other.fechaAlta) {
+    nombre = new char[strlen(other.nombre) + 1];
+    strcpy(nombre, other.nombre);
+}
+
+// Destructor
+Cliente::~Cliente() {
+    delete[] nombre;
+}
+
+// Setter para el nombre
+void Cliente::setNombre(const char* name) {
+    delete[] nombre;
+    nombre = new char[strlen(name) + 1];
+    strcpy(nombre, name);
+}
+
+// Operador de igualdad
+bool Cliente::operator==(const Cliente& c) const {
+    return dni == c.dni;
+}
+
+// Operador de asignación
+Cliente& Cliente::operator=(const Cliente& c) {
+    if (this != &c) {
+        dni = c.dni;
+        fechaAlta = c.fechaAlta;
+
+        delete[] nombre;
+        nombre = new char[strlen(c.nombre) + 1];
+        strcpy(nombre, c.nombre);
+    }
+    return *this;
+}
+
+// Sobrecarga del operador de inserción para imprimir la información del cliente
+std::ostream& operator<<(std::ostream& os, const Cliente& c) {
+    os << "DNI: " << c.dni << " - Nombre: " << c.nombre << " - Fecha de alta: " << c.fechaAlta;
+    return os;
+}
--- Practica 1 MP/Fecha.h
+++ Practica 1 MP/Fecha.h
@@ -1,12 +1,36 @@
 #pragma once
-class Fecha
-{
+#include <iostream>
+using namespace std;
+
+class Fecha {
 private:
-	int day;
-	int month;
-	int year;
+    int dia;
+    int mes;
+    int anio;
 
 public:
-	int getDay() { return day; }
-};
+    // Constructores
+    Fecha(const int day, const int month, const int year);
+    Fecha();
 
+    // Getters y Setters
+    int getDia() const { return dia; }
+    int getMes() const { return mes; }
+    int getAnio() const { return anio; }
+    void setDia(const int day) { dia = day; }
+    void setMes(const int month) { mes = month; }
+    void setAnio(const int year) { anio = year; }
+    void setFecha(const int day, const int month, const int year);
+
+    // Métodos
+    void ver() const;
+    bool bisiesto() const;
+
+    // Sobrecarga de operadores
+    Fecha& operator=(const Fecha& f);
+    Fecha& operator++();      // Pre-incremento
+    Fecha operator++(int);    // Post-incremento
+    friend ostream& operator<<(ostream& os, const Fecha& f);
+    friend const Fecha operator+(const Fecha& f, int dias);
+    friend const Fecha operator+(int dias, const Fecha& f);
+};
